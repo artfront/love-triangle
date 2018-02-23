c43c79590b83b289ce162fda0ad7b739fb306f96
artfront <krivonosart19@gmail.com>
2018-02-23 12:44:49 +0300

    triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+        var arrRight = [];
+        var count = 0;
+        for (var l = 0; l < preferences.length; l++) {
+            arrRight[l+1]=preferences[l];
+        }
+        for (var i = 1; i < arrRight.length; i++) {
+            for (var j = 1; j < arrRight.length; j++) {
+                for (var k = 1; k < arrRight.length; k++) {
+                    if ((j===arrRight[i])&&(k===arrRight[j])&&(i===arrRight[k])&&(arrRight[j]!==arrRight[k])) {count++;}
+                }
+            }
+        }
+        return count/3;
 };
